c8e7878844e27935db42934d017eeefb892e33a4
LYUYJ <1428114705@qq.com>
2025-08-25 19:59:16 +0800

    upgraded the animation function,now animation has left and right these two status.limited the move rangement of the player

--- survivor_like_demo/survivor_like.cpp
+++ survivor_like_demo/survivor_like.cpp
@@ -1,13 +1,20 @@
 #include <graphics.h>
 #include <string>
+#include <vector>
 
 int index_current_anim = 0;
 
 const int PLAYER_ANIM_NUM = 6;
-const int PLAYER_SPEED = 5;
+const int PLAYER_SPEED = 3;
+const int PLAYER_WIDTH = 80;
+const int PLAYER_HEIGHT = 80;
+const int SHADOW_WIDTH = 32;
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 720;
 
 IMAGE img_player_left[PLAYER_ANIM_NUM];
 IMAGE img_player_right[PLAYER_ANIM_NUM];
+IMAGE img_shadow;
 
 POINT player_pos = { 500, 500 };
 
@@ -19,26 +26,85 @@ inline void putimage_alpha(int x, int y, IMAGE* img) {
     AlphaBlend(GetImageHDC(NULL), x, y, w, h,
         GetImageHDC(img), 0, 0, w, h, { AC_SRC_OVER, 0, 255, AC_SRC_ALPHA });
 }
-void loadAnimation() {
-    for (size_t i = 0; i < PLAYER_ANIM_NUM; i++) {
-        std::wstring path = L"img/player_left_" + std::to_wstring(i) + L".png";
-        loadimage(&img_player_left[i], path.c_str());
-    }
-    for (size_t i = 0; i < PLAYER_ANIM_NUM; i++) {
-        std::wstring path = L"img/player_right_" + std::to_wstring(i) + L".png";
-        loadimage(&img_player_right[i], path.c_str());
-    }
-}
-//void loadAnimation() {
-//    IMAGE img_player_sheet;
-//    loadimage(&img_player_sheet, L"image/Idle-Sheet.png");  // 加载包含所有动画帧的图像
-//
-//    // 假设每帧的宽度为256，高度为80，且帧数为6
+
+//此函数已弃用
+// void loadAnimation() {
 //    for (size_t i = 0; i < PLAYER_ANIM_NUM; i++) {
-//        // 提取图像的每一帧
-//        getimage(&img_player_left[i], i * 256, 0, 256, 80);  // 从图像中获取每一帧
+//        std::wstring path = L"img/player_left_" + std::to_wstring(i) + L".png";
+//        loadimage(&img_player_left[i], path.c_str());
+//    }
+//    for (size_t i = 0; i < PLAYER_ANIM_NUM; i++) {
+//        std::wstring path = L"img/player_right_" + std::to_wstring(i) + L".png";
+//        loadimage(&img_player_right[i], path.c_str());
 //    }
 //}
+
+class Animation {
+public:
+    Animation(LPCTSTR path, int num, int interval) {
+        interval_ms = interval;
+
+        //加载图片，因为素材中的命名规律遂将路径参数作为字符串格式化的模板
+        TCHAR path_file[256];
+        for (size_t i = 0; i < num; i++) {
+            _stprintf_s(path_file, path, i);
+
+            IMAGE* frame = new IMAGE();
+            loadimage(frame, path_file);
+            frame_list.push_back(frame);
+        }
+    }
+
+    //动画对象的析构函数
+    ~Animation() {
+        for (size_t i = 0; i < frame_list.size(); i++) {
+            delete(frame_list[i]);
+        }
+    }
+    /*
+    @param delta:距离上次调用Play函数过去了多久
+    */
+    void Play(int x, int y, int delta) {
+        timer += delta;
+        if (timer >= interval_ms) {
+            index_frame = (index_frame + 1) % frame_list.size();
+            timer = 0;
+        }
+
+        putimage_alpha(x, y, frame_list[index_frame]);
+    }
+private:
+    int timer = 0;  //动画计时器
+    int index_frame = 0;    //动画帧索引
+    int interval_ms = 0;    //帧间隔
+    std::vector<IMAGE*> frame_list;
+};
+
+Animation anim_left_player(_T("img/player_left_%d.png"), 6, 45);
+Animation anim_right_player(_T("img/player_right_%d.png"), 6, 45);
+
+void DrawPlayer(int delta, int dir_x) {
+
+    int pos_shadow_x = player_pos.x + (PLAYER_WIDTH / 2 - SHADOW_WIDTH / 2);
+    int pos_shadow_y = player_pos.y + PLAYER_HEIGHT - 8;
+    putimage_alpha(pos_shadow_x, pos_shadow_y, &img_shadow);
+
+    static bool is_facing_left = false;
+    if (dir_x < 0) {
+        is_facing_left = true;
+    }
+    else if (dir_x > 0) {
+        is_facing_left = false;
+    }
+
+    if (is_facing_left == true) {
+        anim_left_player.Play(player_pos.x, player_pos.y, delta);
+    }
+    else {
+        anim_right_player.Play(player_pos.x, player_pos.y, delta);
+    }
+}
+
 int main(void) {
     initgraph(1280, 720);
 
@@ -51,9 +117,12 @@ int main(void) {
     bool is_move_left = false;
     bool is_move_right = false;
 
-    loadAnimation();
+    //更新了新的函数实现
+    // loadAnimation()（已弃用）;
     // 加载背景图
     loadimage(&image_background, _T("img/background.png"));
+    // 加载阴影
+    loadimage(&img_shadow, _T("img/shadow_player.png"));
 
     BeginBatchDraw();
 
@@ -108,10 +177,25 @@ int main(void) {
         // 动画的循环播放
         index_current_anim %= PLAYER_ANIM_NUM;
 
+        int dir_x = is_move_right - is_move_left;
+        int dir_y = is_move_down - is_move_up;
+        double len_dir = sqrt(dir_x * dir_x + dir_y * dir_y);
+        if (len_dir != 0) {
+            double normalized_x = dir_x / len_dir;
+            double normalized_y = dir_y / len_dir;
+            player_pos.x += (int)(PLAYER_SPEED * normalized_x);
+            player_pos.y += (int)(PLAYER_SPEED * normalized_y);
+        }
+        if (player_pos.x < 0) player_pos.x = 0;
+        if (player_pos.y < 0) player_pos.y = 0;
+        if (player_pos.x + PLAYER_WIDTH > WINDOW_WIDTH) player_pos.x = WINDOW_WIDTH - PLAYER_WIDTH;
+        if (player_pos.y + PLAYER_HEIGHT > WINDOW_HEIGHT) player_pos.y = WINDOW_HEIGHT - PLAYER_HEIGHT;
+
         cleardevice();
 
         putimage(0, 0, &image_background);
-        putimage_alpha(player_pos.x, player_pos.y, &img_player_left[index_current_anim]);
+        /*putimage_alpha(player_pos.x, player_pos.y, &img_player_left[index_current_anim]);*/
+        DrawPlayer(1000 / 144, is_move_right - is_move_left);
 
         FlushBatchDraw();
 
